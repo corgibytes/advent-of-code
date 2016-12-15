478206bef6bf6a3aa77f645a13ebd065bf20a336
David Grieser <david.grieser@gmail.com>
2016-12-15 16:08:21 -0600

    logic for handing with matching

--- day_nine.rb
+++ day_nine.rb
@@ -11,6 +11,17 @@ class DayNine
     x_index = @member_variable.index('x')
     return @member_variable if x_index.nil?
 
+    grouped = /(\(\d+x\d+\))/
+    matched_markers = @member_variable.scan(grouped) # Array of arrays with the matching markers
+    # Find index of each marker
+    matched_markers.each do |marker|
+      beginning_index_of_marker = @member_variable.index(marker.first)
+    end
+    # For Fifth Test Case skip markers - if index is within first matching pattern
+
+    marker_match = /\((\d+)x(\d+)\)/
+    number_of_characters, repeat_count = @member_variable.match(marker_match)
+
     bracket_index = x_index - 2
     new_string = @member_variable[0..bracket_index-1]
     repeat_count = @member_variable[x_index+1].to_i
