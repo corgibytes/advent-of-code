97dbcbd9651f6794049b1103426442c503cb97aa
David Grieser <david.grieser@gmail.com>
2016-12-15 15:45:53 -0600

    initial commit with 2 solutions

---
+++ day_nine.rb
@@ -0,0 +1,26 @@
+class DayNine
+  def initialize(input_string)
+    @member_variable = input_string
+  end
+
+  def result_length
+    result.length
+  end
+
+  def result
+    x_index = @member_variable.index('x')
+    return @member_variable if x_index.nil?
+
+    bracket_index = x_index - 2
+    new_string = @member_variable[0..bracket_index-1]
+    repeat_count = @member_variable[x_index+1].to_i
+    first_character_after_ending_bracket_index = x_index + 3
+    remaining_character_index = x_index + 4
+
+    repeat_count.times do
+      new_string += @member_variable[first_character_after_ending_bracket_index]
+    end
+
+    new_string += @member_variable[remaining_character_index]
+  end
+end
