2a24badf3ce9beb34d7e7ed0d8dfb45e12185046
David Grieser <david.grieser@gmail.com>
2016-12-19 14:09:19 -0600

    drop regex mapping

--- 2016/day_nine/day_nine.rb
+++ 2016/day_nine/day_nine.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class DayNine
   def initialize(input_string)
     @member_variable = input_string
@@ -10,18 +12,6 @@ class DayNine
   def result
     x_index = @member_variable.index('x')
     return @member_variable if x_index.nil?
-
-    grouped = /(\(\d+x\d+\))/
-    matched_markers = @member_variable.scan(grouped) # Array of arrays with the matching markers
-    # Find index of each marker
-    matched_markers.each do |marker|
-      beginning_index_of_marker = @member_variable.index(marker.first)
-    end
-    # For Fifth Test Case skip markers - if index is within first matching pattern
-
-    marker_match = /\((\d+)x(\d+)\)/
-    number_of_characters, repeat_count = @member_variable.match(marker_match)
-
     bracket_index = x_index - 2
     new_string = @member_variable[0..bracket_index-1]
     repeat_count = @member_variable[x_index+1].to_i
