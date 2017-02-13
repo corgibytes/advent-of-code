c044bccc5f5ae0c9bb04beb7beb2fc8805224643
David Grieser <david.grieser@gmail.com>
2017-02-13 14:12:52 -0600

    add input for day 1
    adjust steps to include non single digit counts

--- 2016/day_one/test_day_one.py
+++ 2016/day_one/test_day_one.py
@@ -12,7 +12,8 @@ def distance_from_center(directions):
 
   for direction_set in direction_array:
     turn = direction_set[0]
-    steps = int(direction_set[1])
+    steps_string = ''.join(str(x) for x in direction_set[1:len(direction_set)])
+    steps = int(steps_string)
     # From Facing "North" - R == "East" L == "West"
     if facing == "North":
       if turn == "R":
@@ -63,3 +64,6 @@ def test_instruction_two():
   # Followed L3
   # Facing "North"
   # (3, 2)
+
+def test_input_instructions():
+  assert distance_from_center("L2, L5, L5, R5, L2, L4, R1, R1, L4, R2, R1, L1, L4, R1, L4, L4, R5, R3, R1, L1, R1, L5, L1, R5, L4, R2, L5, L3, L3, R3, L3, R4, R4, L2, L5, R1, R2, L2, L1, R3, R4, L193, R3, L5, R45, L1, R4, R79, L5, L5, R5, R1, L4, R3, R3, L4, R185, L5, L3, L1, R5, L2, R1, R3, R2, L3, L4, L2, R2, L3, L2, L2, L3, L5, R3, R4, L5, R1, R2, L2, R4, R3, L4, L3, L1, R3, R2, R1, R1, L3, R4, L5, R2, R1, R3, L3, L2, L2, R2, R1, R2, R3, L3, L3, R4, L4, R4, R4, R4, L3, L1, L2, R5, R2, R2, R2, L4, L3, L4, R4, L5, L4, R2, L4, L4, R4, R1, R5, L2, L4, L5, L3, L2, L4, L4, R3, L3, L4, R1, L2, R3, L2, R1, R2, R5, L4, L2, L1, L3, R2, R3, L2, L1, L5, L2, L1, R4") == 181
